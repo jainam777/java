ac1d59d940a53e75b2056da01c45f6d5d380dd6d
jainam.gala@westpac.com.au <jainam.gala@westpac.com.au>
2025-04-13 18:39:02 +0530

    Star Pattern Update

--- JavaConcepts/src/arrays/StarPattern.java
+++ JavaConcepts/src/arrays/StarPattern.java
@@ -35,8 +35,8 @@ public class StarPattern {
 		System.out.println();
 		System.out.println("Pyramind Angled Trainagle");
 		
-		for(int i=0;i<arr.length;i++) {
-			for(int j = 0; j<arr.length-i; j++) {
+		for(int i=1;i<arr.length;i++) {
+			for(int j = 0; j<arr.length-i-1; j++) {
 				System.out.print(" ");
 			}
 			for(int k=0;k<2*i-1;k++) {
@@ -58,6 +58,27 @@ public class StarPattern {
 				System.out.print("*");
 			}
 			System.out.println("");
+		}
+		
+		System.out.println();
+		System.out.println("Daimond");
+		for(int i=1;i<=arr.length;i++) {
+			for(int j=0;j<arr.length-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<2*i-1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int a=arr.length-1;a>0;a--) {
+			for(int b=0;b<arr.length-a;b++) {
+				System.out.print(" ");
+			}
+			for(int c=0;c<2*a-1;c++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 		
 	}
